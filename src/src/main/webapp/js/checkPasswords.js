340e137d4cf1d4acbcf14302f88a32b41bd833dd
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2024-07-24 10:16:51 -0300

    : :recycle: refactor:  Refatorando métodos

--- src/src/main/webapp/js/checkPasswords.js
+++
@@ -1,8 +0,0 @@
-function checkPasswords(){
-    if (document.getElementById("password").value !== document.getElementById("confirm-password").value) {
-        alert("As senhas não coincidem!")
-        return false
-    } else {
-        return true
-    }
-}
